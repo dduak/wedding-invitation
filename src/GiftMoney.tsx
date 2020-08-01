e90381def7a3786d337e40727f7a58e876299ce6
Jinwoo Oh <arkist@gmail.com>
2020-08-01 18:38:49 +0900

    Remove TossButton
    because urls are outdated(expired).

--- src/GiftMoney.tsx
+++ src/GiftMoney.tsx
@@ -10,36 +10,17 @@ const GiftMoney: React.FC = props => {
     <section>
       <h1>마음 전하는 곳</h1>
       신랑<br/>
-      <TossButton type={Person.Ark}/>
       <KakaoPayButton type={Person.Ark}/>
       {/*신랑측 부*/}
       {/*신랑측 모*/}
 
       <br/>
       신부<br/>
-      <TossButton type={Person.Ddugi}/>
       <KakaoPayButton type={Person.Ddugi}/>
       {/*신부측 부*/}
       {/*신부측 모*/}
 
     </section>
-  )
-}
-
-const TossButton: React.FC<{ type: Person }> = props => {
-  const mapping = {
-    [Person.Ark]: 'JTdfWXGb',
-    [Person.Ddugi]: 'tLXbCOr5'
-  }
-
-  return (
-    <a className="gift-button" href={`https://toss.im/_m/${mapping[props.type]}`}>
-      <img
-        src="/toss_200_100.png"
-        alt="토스로 축의금 보내기"
-        height={25}
-      />
-    </a>
   )
 }
 
